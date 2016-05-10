eeec9d27349d37990f4b4d1190bc6452d1c3ca59
Eric Rasche <rasche.eric@gmail.com>
2016-05-10 14:58:07 +0000

    Add new script to unshare folders

---
+++ unshare.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#set -ex
+PARENT_FOLDER=****************************
+parents=$(mktemp)
+
+gdrive share list $PARENT_FOLDER | grep '@tamu.edu' | awk '{print $4}' > $parents
+
+echo $parent_folder_emails
+
+function unshare(){
+    STUDENT_NAME=$1
+    STUDENT_EMAIL=$2
+
+    folders=$(gdrive list  -q '"'$PARENT_FOLDER'" in parents')
+    for id in $(echo "$folders" | awk '(NR>1){print $1}');
+    do
+        data=$(gdrive share list $id | grep '@tamu.edu' | grep -v -f $parents)
+        share_ids=($(echo "$data"|  awk '{print $1}'))
+        emails=($(echo "$data"|  awk '{print $4}'))
+        #for share_id in $share_ids; do
+        for ((i=0;i<${#share_ids[@]};i++)); do
+            email=${emails[i]}
+            share_id=${share_ids[i]}
+            echo "Unsharing $id from $email ($share_id)"
+            gdrive share revoke $id $share_id;
+        done
+    done
+}
+unshare
+rm $parents
